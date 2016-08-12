28b1ca3c6326b646aae8eb771dbb08016309eb18
Bryan Jiang <BryanJiang@Bryans-MacBook-Air.local>
2016-08-11 20:20:19 -0400

    Initial commit

---
+++ app/src/main/java/com/example/bryanjiang/focus/FinishedFragment.java
@@ -0,0 +1,22 @@
+package com.example.bryanjiang.focus;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class FinishedFragment extends Fragment {
+
+
+    public FinishedFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_finished, container, false);
+    }
+}
